3bc430fb1dfa4405f79b25aaed3ad8889b6f3554
Artem Gusev <117298408+agusev2311@users.noreply.github.com>
2025-03-27 18:26:17 +0300

    Flask infrostructure

--- backend/application/infrastructure.py
+++ backend/application/infrastructure.py
@@ -1,3 +1,6 @@
+from flask import app
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -8,6 +11,10 @@ db = SQLAlchemy()
 jwt = JWTManager()
 bcrypt = Bcrypt()
 mail = Mail()
+limiter = Limiter(
+    get_remote_address,
+    storage_uri="memory://",
+)
 celery = Celery(
     "school_crm_app",
     result_expires=600,
